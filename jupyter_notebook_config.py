4d0e286f4e3f949401df8c24ab75d71ee80ec1d6
Amy Lu <amyxlu@cs.toronto.edu>
2020-03-04 13:11:10 -0500

    add jupyter config for clusters

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,4 @@
+c = get_config()
+c.NotebookApp.ip = "foobar"   # IP here 
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = "foobar" # Set different ports for different clusters for convenience
